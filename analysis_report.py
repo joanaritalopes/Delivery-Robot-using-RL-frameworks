1b2fdef2c1dcdb4760d6e49634b59401fe8d1bfb
Kwan-Lam <83657762+Kwan-Lam@users.noreply.github.com>
2026-06-21 22:08:35 +0200

    Remove hard coding 2

--- analysis_report.py
+++ analysis_report.py
@@ -429,8 +429,6 @@ def parse_agent_grid_from_filename(path):
 
 
 def plot_convergence_curves(results_dir, plots_dir, smoothing=10):
-    results_dir = Path("results")
-
     eval_files = list(results_dir.glob("*_eval.csv"))
     train_files = list(results_dir.glob("*_train.csv"))
 
